29fe6ce81ff28877e51b43141c20f9d6f0c92b66
PandaFeeder <pandafeeder@gmail.com>
2017-08-07 02:43:22 +0800

    kNN

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -7,17 +7,6 @@ def createDateSet():
     labels = np.array([['A'], ['A'], ['B'], ['B']])
     return group, labels
 
-group, labels = createDateSet()
-
-# plotting
-fig = plt.figure()
-ax = fig.add_subplot(111)
-x = [i[0] for i in group]
-y = [i[1] for i in group]
-ax.scatter(x, y)
-for i, l in enumerate(labels):
-    ax.annotate(l[0], (x[i]-0.05, y[i]))
-#plt.show()
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
@@ -33,4 +22,49 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-print(classify0([0, 0], group, labels, 3))
+
+def file2Matrix(filename):
+    with open(filename, 'r') as fd:
+        count = len(fd.readlines())
+    initMat = np.zeros([count, 3])
+    labelList = []
+    with open(filename, 'r') as fd:
+        lineNum = 0
+        for line in fd:
+            line = line.strip()
+            record = line.split('\t')
+            initMat[lineNum] = record[:3]
+            labelList.append(record[-1])
+            lineNum += 1
+    return initMat, np.array(labelList)
+
+
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(dataSet.shape)
+    m = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+if __name__ == '__main__':
+    group, labels = createDateSet()
+    # plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x = [i[0] for i in group]
+    y = [i[1] for i in group]
+    ax.scatter(x, y)
+    for i, l in enumerate(labels):
+        ax.annotate(l[0], (x[i]-0.05, y[i]))
+    data, label = file2Matrix('datingTestSet.txt')
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+    ax2.scatter(data[:,1], data[:,2])
+
+    plt.show()
